2a25fd6137f99d1de76352361769883940f65405
Alsa2 <alsa0622@hotmail.com>
2023-02-23 20:00:33 +0900

    i give up on this shit without copilot Why no wifis in shikansens :(

--- DB/schemas.py
+++ DB/schemas.py
@@ -4,16 +4,6 @@ from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
 
-class Menu(Base):
-    __tablename__ = 'menu'
-    id = Column(Integer, primary_key=True)
-    date = Column(String) #date
-    meal = Column(Integer) # 1, 2, 3
-    edition = Column(String) #datetime
-    notes = Column(String) #additional information (e.g. "vegetarian")
-    dish_id = Column(Integer, ForeignKey('dish.id'))
-    dish = relationship("Dish", back_populates="menu")
-
 class Dish(Base):
     __tablename__ = 'dish'
     id = Column(Integer, primary_key=True)
@@ -32,3 +22,19 @@ class DishStats(Base):
     likes = Column(Integer)
     dislikes = Column(Integer)
     neutrals = Column(String)
+
+class FeedBack(Base):
+    __tablename__ = 'feedback'
+    id = Column(Integer, primary_key=True)
+    
+
+
+class Menu(Base):
+    __tablename__ = 'menu'
+    id = Column(Integer, primary_key=True)
+    date = Column(String) #date
+    meal = Column(Integer) # 1, 2, 3
+    edition = Column(String) #datetime
+    notes = Column(String) #additional information (e.g. "vegetarian")
+    dish_id = Column(Integer, ForeignKey('dish.id'))
+    dish = relationship("Dish", back_populates="menu")
